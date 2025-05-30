38f72c9f56694fc1c4d85cf2fe32e5043fd76ef2
Angelina Paramitha <angelinaparamitha385@gmail.com>
2025-05-30 18:05:38 +0700

    ModeldanControllerPelangganMerkKendaraanVarian

---
+++ app/Http/Controllers/MerkController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\merk;
+use Illuminate\Http\Request;
+
+class MerkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(merk $merk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(merk $merk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, merk $merk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(merk $merk)
+    {
+        //
+    }
+}
