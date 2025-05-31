ac8f493ab83ba1d507cf968e4d795d3e967ed88c
davinastra777 <davinastra_2327240037@mhs.mdp.ac.id>
2025-05-31 09:45:24 +0700

    Update show.blade.php

--- resources/views/kendaraan/show.blade.php
+++ resources/views/kendaraan/show.blade.php
@@ -8,7 +8,7 @@
         <!-- Default box -->
         <div class="card">
           <div class="card-header">
-            <h3 class="card-title">List Kendaraan</h3>
+            <h3 class="card-title">List Kendaraann</h3>
             <div class="card-tools">
               <button
                 type="button"
